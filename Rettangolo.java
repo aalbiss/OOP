e4c7cb3f824d630b834511563b120542ac40c3ac
aalbiss <121013006+aalbiss@users.noreply.github.com>
2023-03-18 08:01:18 +0100

    Update Rettangolo.java

--- Rettangolo.java
+++ Rettangolo.java
@@ -4,35 +4,35 @@ public class Rettangolo {
 
 	int base;
 	int altezza;
-	
+
 	Rettangolo(){
 		base=0;
 		altezza=0;
 	}
-	
+
 	Rettangolo(int b, int a){
-		b = base;
-		a = altezza;
+		base = b;
+		altezza = a;
 	}
-	
+
 	int perimetro() {
 		int p = (base+altezza)*2;
 		return p;
 	}
-	
+
 	int area() {
 		int area=base*altezza;
 		return area;
 	}
-	
-	public static void main(System []args) {
-		
+
+	public static void main(String[] args) {
+
 		Rettangolo r = new Rettangolo(10, 5);
-		
+
 		System.out.println(r.perimetro());
 		System.out.println(r.area());
-		
-		
+
+
 	}
-	
+
 }
